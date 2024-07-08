5d96225ffd76c35417de9ae932e9477432eb6427
Harshal Kolhe <harshalkolhe0@gmail.com>
2024-07-08 19:38:11 +0530

    added /somepath

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const port = 3000;
+app.use(cors());
+app.get("/somepath", (req, res) => {
+    console.log("Some path is hitted");
+    res.status(200).json({ hello: "world from /somepath" });
+});
 app.get("*", (req, res) => {
     console.log("Some routes get hit");
-    res.status(200).json({ hello: "world" });
+    res.status(200).json({ hello: "world from *" });
 });
 app.listen(port, () => {
     console.log("listening on port 3000");
